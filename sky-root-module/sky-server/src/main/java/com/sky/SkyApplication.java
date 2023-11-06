6575987d82614590378b2a9246a6dd37eb8d1ef4
lu55by <1738212274@qq.com>
2023-11-06 12:02:15 +0800

    Initialized Sky project.

---
+++ sky-root-module/sky-server/src/main/java/com/sky/SkyApplication.java
@@ -0,0 +1,20 @@
+//Date: 2023/11/6
+//Time: 11:50
+
+
+package com.sky;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Slf4j
+@SpringBootApplication
+@EnableTransactionManagement
+public class SkyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SkyApplication.class, args);
+        log.info("SkyApplication started");
+    }
+}
